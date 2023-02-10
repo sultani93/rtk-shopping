a564e0778be69535a5ddd4f57ff43dea42a67793
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2023-02-10 16:28:34 +0400

    at least createAsyncThunk created and in slicer i used extrareducers with builder and in here state gets data from api with help of action.payload

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 export const STATUSES = Object.freeze({
   IDLE: 'idle',
@@ -14,15 +14,28 @@ export const productSlice = createSlice({
   },
 
   reducers: {
-    setProducts(state, action) {
-      state.data = action.payload
-    },
-    setStatus(state, action) {
-      state.status = action.payload
-    },
+    // setProducts(state, action) {
+    //   state.data = action.payload
+    // },
+    // setStatus(state, action) {
+    //   state.status = action.payload
+    // },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = STATUSES.LOADING
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.data = action.payload
+        state.status = STATUSES.IDLE
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = STATUSES.ERROR
+      })
   },
 })
-export const { setProducts, setStatus } = productSlice.actions
+// export const { setProducts, setStatus } = productSlice.actions
 export default productSlice.reducer
 
 // export function fetchProducts() {
@@ -41,3 +54,9 @@ export default productSlice.reducer
 // }
 
 // thunks
+
+export const fetchProducts = createAsyncThunk('/products/fetch', async () => {
+  const res = await fetch('https://fakestoreapi.com/products')
+  const data = await res.json()
+  return data
+})
